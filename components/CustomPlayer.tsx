3976a963d5e06ef0e51313cfa36d42270db9e754
nicolascastagna <100592012+nicolascastagna@users.noreply.github.com>
2023-01-02 16:10:11 +0100

    sécurité si requête non disponible

--- components/CustomPlayer.tsx
+++ components/CustomPlayer.tsx
@@ -3,6 +3,7 @@
 import { useRecoilState } from "recoil";
 import { modalState, movieState } from "../atoms/movieState";
 import ReactPlayer from "react-player";
+import Link from "next/link";
 
 const CustomPlayer = () => {
   const [modal, setModal] = useRecoilState(modalState);
@@ -16,7 +17,7 @@ const CustomPlayer = () => {
       } fixed inset-0 z-30 bg-black bg-opacity-50`}
     >
       <div className="w-full h-full flex flex-col justify-center items-center relative">
-        {modal && selectedMovie[0]?.key && (
+        {modal && selectedMovie[0]?.key ? (
           <ReactPlayer
             url={`https://www.youtube.com/watch?v=${selectedMovie[0]?.key}`}
             width="80%"
@@ -26,6 +27,15 @@ const CustomPlayer = () => {
             playing
             controls={true}
           />
+        ) : (
+          <div className="fixed flex flex-col text-center items-center space-x-1 text-lg pt-5">
+            Aucun contenu disponible
+            <Link href="/">
+              <button className="border-solid border-2 border-white p-3 mt-5">
+                Cliquer pour actualiser
+              </button>
+            </Link>
+          </div>
         )}
       </div>
     </div>
